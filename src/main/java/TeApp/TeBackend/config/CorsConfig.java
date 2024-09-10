052ede66c7c20bf8a70c5ac1b725e9fb61ea0b59
tsmanjusha5 <tellasrimanjusha@gmail.com>
2024-09-10 00:21:30 -0500

    config file update

--- src/main/java/TeApp/TeBackend/config/CorsConfig.java
+++ src/main/java/TeApp/TeBackend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://teachingeval.netlify.app/")
+                .allowedOrigins("https://teachingeval.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
